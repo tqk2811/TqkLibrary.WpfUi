88b79c45a35fbea9ecf7517954a246c2b36e8e5e
tqk2811 <tqk2811@gmail.com>
2021-12-28 16:02:25 +0700

    add NullToFalseConverter

---
+++ TqkLibrary.WpfUi/Converters/NullToFalseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TqkLibrary.WpfUi.Converters
+{
+    public class NullToFalseConverter : IValueConverter
+    {
+        public bool Result { get; set; } = false;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val = value != null;//
+            if (val) return val;
+            else return Result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
