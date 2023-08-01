e42f33d80da3c1d977188953bf85024073e1d1da
tqk2811 <tqk2811@gmail.com>
2023-08-01 15:37:20 +0700

    rework SaveObservableCollection

--- TqkLibrary.WpfUi/ObservableCollection/SaveObservableCollection.cs
+++ TqkLibrary.WpfUi/ObservableCollection/SaveObservableCollection.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Timers;
 
 namespace TqkLibrary.WpfUi.ObservableCollection
@@ -16,158 +17,97 @@ namespace TqkLibrary.WpfUi.ObservableCollection
     /// </summary>
     /// <typeparam name="TData"></typeparam>
     /// <typeparam name="TViewModel"></typeparam>
-    public class SaveObservableCollection<TData, TViewModel> : ObservableCollection<TViewModel>
+    public class SaveObservableCollection<TData, TViewModel> : DispatcherObservableCollection<TViewModel>
       where TData : class
       where TViewModel : class, IViewModel<TData>
     {
         /// <summary>
         /// 
         /// </summary>
-        public string SavePath { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
-        public bool IsAutoSave { get; set; } = true;
-        /// <summary>
-        /// 
-        /// </summary>
-        public double Interval
-        {
-            get { return timer.Interval; }
-            set { timer.Interval = value; }
-        }
+        public event Action<IEnumerable<TData>> OnSave;
 
-        private readonly Timer timer;
-        private bool IsLoaded = false;
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="savePath"></param>
+        /// <param name="datas"></param>
         /// <param name="func"></param>
-        /// <param name="interval"></param>
         /// <exception cref="ArgumentNullException"></exception>
-        public SaveObservableCollection(string savePath, Func<TData, TViewModel> func, int interval = 500)
+        public void Load(IEnumerable<TData> datas, Func<TData, TViewModel> func)
         {
-            if (string.IsNullOrEmpty(savePath)) throw new ArgumentNullException(nameof(savePath));
-            this.SavePath = savePath;
-            timer = new Timer(interval)
-            {
-                AutoReset = false
-            };
-            timer.Elapsed += Timer_Elapsed;
-            Load(func);
-        }
+            if (datas is null) throw new ArgumentNullException(nameof(datas));
+            if (func is null) throw new ArgumentNullException(nameof(func));
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="interval"></param>
-        public SaveObservableCollection(int interval = 500)
-        {
-            timer = new Timer(interval)
+            IsLoaded = false;
+            try
             {
-                AutoReset = false
-            };
-            timer.Elapsed += Timer_Elapsed;
-        }
-
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            lock (timer)
+                this.Clear();
+                foreach(var item in datas)
+                {
+                    this.Add(func(item));
+                }
+            }
+            finally
             {
-                using StreamWriter sw = new StreamWriter(SavePath, false);
-                sw.Write(JsonConvert.SerializeObject(this.Select(x => x.Data).ToList(), Formatting.Indented));
+                IsLoaded = true;
             }
         }
+
+        private bool IsLoaded = false;
+
         /// <summary>
         /// 
         /// </summary>
         public void Save()
         {
-            if (IsLoaded)
-            {
-                timer.Stop();
-                timer.Start();
-            }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public void ForceSave()
-        {
-            timer.Stop();
-            try
-            {
-                Timer_Elapsed(null, null);
-            }
-            catch (Exception) { }
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="func"></param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public void Load(Func<TData, TViewModel> func)//Func - Action - Predicate - ....
-        {
-            if (func == null) throw new ArgumentNullException(nameof(func));
-
-            IsLoaded = false;
-            this.Clear();
-            List<TData> t2s = new List<TData>();
-            if (File.Exists(SavePath))
-            {
-                try
-                {
-                    using StreamReader sr = new StreamReader(SavePath);
-                    List<TData> list = JsonConvert.DeserializeObject<List<TData>>(sr.ReadToEnd());
-                    t2s.AddRange(list);
-                }
-                catch (Exception) { }
-            }
-            t2s.ForEach(x => this.Add(func.Invoke(x)));
-            IsLoaded = true;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="SavePath"></param>
-        /// <param name="func"></param>
-        public void Load(string SavePath, Func<TData, TViewModel> func)
-        {
-            this.SavePath = SavePath;
-            Load(func);
+            this.OnSave?.Invoke(this.Select(x => x.Data));
         }
 
         #region ObservableCollection
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
         protected override void InsertItem(int index, TViewModel item)
         {
             item.Change += ItemData_Change;
             base.InsertItem(index, item);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         protected override void ClearItems()
         {
             foreach (var item in this) item.Change -= ItemData_Change;
             base.ClearItems();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
         protected override void RemoveItem(int index)
         {
             this[index].Change -= ItemData_Change;
             base.RemoveItem(index);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
         protected override void SetItem(int index, TViewModel item)
         {
             this[index].Change -= ItemData_Change;
             item.Change += ItemData_Change;
             base.SetItem(index, item);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
-            if (IsAutoSave) Save();
+            if (IsLoaded) Save();
             base.OnCollectionChanged(e);
         }
 
@@ -175,7 +115,7 @@ namespace TqkLibrary.WpfUi.ObservableCollection
 
         private void ItemData_Change(object obj, TData data)
         {
-            if (IsAutoSave) Save();
+            Save();
         }
         /// <summary>
         /// 
