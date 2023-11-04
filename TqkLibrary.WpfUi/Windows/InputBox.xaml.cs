b9fb5b100de7ec006538e69fee0bf74282e064b1
tqk2811 <tqk2811@gmail.com>
2023-11-04 16:51:55 +0700

    update inputbox

--- TqkLibrary.WpfUi/Windows/InputBox.xaml.cs
+++ TqkLibrary.WpfUi/Windows/InputBox.xaml.cs
@@ -1,53 +1,121 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace TqkLibrary.WpfUi.Windows
 {
-  public delegate bool DelegateValidate(string Name);
-  /// <summary>
-  /// Interaction logic for InputBox.xaml
-  /// </summary>
-  public partial class InputBox : Window
-  {
-    public InputBox(string InputDefault = "")
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public delegate void ValidateEventHandler(object sender, ValidateEventArgs e);
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ValidateEventArgs : EventArgs
     {
-      InitializeComponent();
-      this.Tb.Text = InputDefault;
-
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsValid { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ErrorMessage { get; set; } = nameof(ErrorMessage);
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ErrorTitle { get; set; } = "Error";
     }
-
-    public event DelegateValidate Validate;
-
-    public string Result { get; private set; }
-    private void Add_Click(object sender, RoutedEventArgs e)
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox : Window
     {
-      if (Validate?.Invoke(Tb.Text) == true)
-      {
-        Result = Tb.Text;
-        this.Close();
-      }
-      else MessageBox.Show("Item are existed", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        public event ValidateEventHandler Validate;
 
-    private void Cancel_Click(object sender, RoutedEventArgs e)
-    {
-      Result = null;
-      this.Close();
-    }
 
-    private void Tb_KeyDown(object sender, KeyEventArgs e)
-    {
-      if (e.Key == Key.Enter)
-      {
-        Add_Click(null, null);
-      }
-    }
 
-    private void Window_Loaded(object sender, RoutedEventArgs e)
-    {
-      Tb.Focus();
-      Tb.SelectAll();
-      Keyboard.Focus(Tb);
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(InputBox),
+            new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly DependencyProperty TextAlignmentProperty = DependencyProperty.Register(
+            nameof(TextAlignment),
+            typeof(TextAlignment),
+            typeof(InputBox),
+            new FrameworkPropertyMetadata(TextAlignment.Left, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// 
+        /// </summary>
+        public TextAlignment TextAlignment
+        {
+            get { return (TextAlignment)GetValue(TextAlignmentProperty); }
+            set { SetValue(TextAlignmentProperty, value); }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (Validate is not null)
+            {
+                ValidateEventArgs validateEventArgs = new ValidateEventArgs();
+                Validate?.Invoke(this, validateEventArgs);
+                if (!validateEventArgs.IsValid)
+                {
+                    MessageBox.Show(validateEventArgs.ErrorMessage, validateEventArgs.ErrorTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void Tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Add_Click(null, null);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Tb.Focus();
+            Tb.SelectAll();
+            Keyboard.Focus(Tb);
+        }
     }
-  }
 }
