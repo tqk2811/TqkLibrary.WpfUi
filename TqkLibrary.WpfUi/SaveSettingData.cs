5226956cbe080f66aff717629846548563e5af4a
tqk2811 <tqk2811@gmail.com>
2023-11-27 15:05:18 +0700

    update SaveSettingData

--- TqkLibrary.WpfUi/SaveSettingData.cs
+++ TqkLibrary.WpfUi/SaveSettingData.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 
@@ -16,41 +17,57 @@ namespace TqkLibrary.WpfUi
         /// 
         /// </summary>
         public T Setting { get; private set; }
-        private readonly string SavePath;
-        private readonly Timer timer;
+        /// <summary>
+        /// 
+        /// </summary>
+        public double DelaySaving
+        {
+            get { return _timer.Interval; }
+            set { _timer.Interval = value; }
+        }
+        const double _defaultDelaySaving = 500;
         /// <summary>
         /// 
         /// </summary>
         public event Action<T> OnSaved;
+        /// <summary>
+        /// 
+        /// </summary>
+        public event Action<Exception> OnSaveError;
 
+
+        private readonly string _savePath;
+        private readonly System.Timers.Timer _timer;
+        private readonly JsonSerializerSettings _jsonSerializerSettings;
         /// <summary>
         /// 
         /// </summary>
         /// <param name="SavePath"></param>
-        /// <param name="delay"></param>
+        /// <param name="jsonSerializerSettings"></param>
         /// <exception cref="ArgumentNullException"></exception>
-        public SaveSettingData(string SavePath, int delay = 500)
+        public SaveSettingData(string SavePath, JsonSerializerSettings jsonSerializerSettings = null)
         {
             if (string.IsNullOrEmpty(SavePath)) throw new ArgumentNullException(nameof(SavePath));
-            this.SavePath = SavePath;
-            if (File.Exists(SavePath)) Setting = JsonConvert.DeserializeObject<T>(File.ReadAllText(SavePath));
+            this._jsonSerializerSettings = jsonSerializerSettings;
+            this._savePath = SavePath;
+            if (File.Exists(SavePath)) Setting = JsonConvert.DeserializeObject<T>(File.ReadAllText(SavePath), jsonSerializerSettings);
             else Setting = new T();
-            timer = new Timer(delay);
-            timer.Elapsed += Timer_Elapsed;
-            timer.AutoReset = false;
+            _timer = new System.Timers.Timer(_defaultDelaySaving);
+            _timer.Elapsed += Timer_Elapsed;
+            _timer.AutoReset = false;
         }
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             try
             {
-                File.WriteAllText(SavePath, JsonConvert.SerializeObject(Setting, Formatting.Indented));
+                File.WriteAllText(_savePath, JsonConvert.SerializeObject(Setting, Formatting.Indented, _jsonSerializerSettings));
+                OnSaved?.Invoke(Setting);
             }
-            catch
+            catch (Exception ex)
             {
-
+                OnSaveError?.Invoke(ex);
             }
-            OnSaved?.Invoke(Setting);
         }
 
         /// <summary>
@@ -58,27 +75,31 @@ namespace TqkLibrary.WpfUi
         /// </summary>
         public void Save()
         {
-            timer?.Stop();
-            timer?.Start();
+            _timer.Stop();
+            _timer.Start();
         }
 
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public async Task SaveAsync()
+        public async Task<T> SaveAsync(CancellationToken cancellationToken = default)
         {
-            TaskCompletionSource<bool> result = new TaskCompletionSource<bool>();
-            ElapsedEventHandler action = (object sender, ElapsedEventArgs e) => result.TrySetResult(true);
-            Save();
+            TaskCompletionSource<T> result = new TaskCompletionSource<T>();
+            Action<T> onSaved = (o) => result.TrySetResult(o);
+            Action<Exception> onError = (e) => result.TrySetException(e);
+            using var register = cancellationToken.Register(() => result.TrySetCanceled());
             try
             {
-                timer.Elapsed += action;
-                await result.Task.ConfigureAwait(false);
+                OnSaved += onSaved;
+                OnSaveError += onError;
+                Save();
+                return await result.Task.ConfigureAwait(false);
             }
             finally
             {
-                timer.Elapsed -= action;
+                OnSaved -= onSaved;
+                OnSaveError -= onError;
             }
         }
     }
