a3f5a0be0ce64bad70af2dd05af277893911be45
tqk2811 <tqk2811@gmail.com>
2023-11-30 16:42:54 +0700

    rework SaveGroupObservableCollection

--- TqkLibrary.WpfUi/ObservableCollections/SaveGroupObservableCollection.cs
+++ TqkLibrary.WpfUi/ObservableCollections/SaveGroupObservableCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace TqkLibrary.WpfUi.ObservableCollections
@@ -15,12 +16,36 @@ namespace TqkLibrary.WpfUi.ObservableCollections
       where TData : class, IItemData<TId>
       where TViewModel : class, IViewModel<TData>
     {
-        readonly List<TViewModel> _datas = new List<TViewModel>();
+        readonly Dictionary<TId, List<TViewModel>> _dict_datas = new Dictionary<TId, List<TViewModel>>();
+
+        List<TViewModel> _EnsureCreateDictKey(TId key)
+        {
+            this.Dispatcher.VerifyAccess();
+            if (!_dict_datas.ContainsKey(key))
+                _dict_datas.Add(key, new List<TViewModel>());
+            return _dict_datas[key];
+        }
+        void _CleanEmptyDict()
+        {
+            this.Dispatcher.VerifyAccess();
+            foreach (var pair in _dict_datas.Where(x => x.Value.Count == 0).ToList())
+            {
+                _dict_datas.Remove(pair.Key);
+            }
+        }
 
         /// <summary>
         /// 
         /// </summary>
-        public IEnumerable<TViewModel> ViewModels => _datas;
+        public IEnumerable<TViewModel> ViewModels => _dict_datas.SelectMany(x => x.Value);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void ClearAll()
+        {
+
+        }
 
         /// <summary>
         /// 
@@ -32,9 +57,13 @@ namespace TqkLibrary.WpfUi.ObservableCollections
         {
             if (datas is null) throw new ArgumentNullException(nameof(datas));
             if (func is null) throw new ArgumentNullException(nameof(func));
+            this.Dispatcher.VerifyAccess();
 
-            _datas.Clear();
-            _datas.AddRange(datas.Select(func));
+            _dict_datas.Clear();
+            foreach (var data in datas)
+            {
+                _EnsureCreateDictKey(data.GroupId).Add(func(data));
+            }
         }
 
         /// <summary>
@@ -45,25 +74,21 @@ namespace TqkLibrary.WpfUi.ObservableCollections
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
         public override Task LoadAsync(IEnumerable<TData> datas, Func<TData, TViewModel> func)
-        {
-            if (datas is null) throw new ArgumentNullException(nameof(datas));
-            if (func is null) throw new ArgumentNullException(nameof(func));
-
-            _datas.Clear();
-            _datas.AddRange(datas.Select(func));
-
-            return Task.CompletedTask;
-        }
+            => this.Dispatcher.TrueThreadInvokeAsync(() => Load(datas, func));
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="func"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public virtual void Show(Func<TViewModel, bool> func)
         {
+            if (func is null) throw new ArgumentNullException(nameof(func));
+            this.Dispatcher.VerifyAccess();
+
             using var l = GetLockLoader();
             this.Clear();
-            foreach (var data in _datas)
+            foreach (var data in _dict_datas.SelectMany(x => x.Value))
             {
                 if (func(data))
                     this.Add(data);
@@ -75,16 +100,8 @@ namespace TqkLibrary.WpfUi.ObservableCollections
         /// </summary>
         /// <param name="func"></param>
         /// <returns></returns>
-        public virtual async Task ShowAsync(Func<TViewModel, bool> func)
-        {
-            using var l = GetLockLoader();
-            await this.ClearAsync();
-            foreach (var data in _datas)
-            {
-                if (func(data))
-                    await this.AddAsync(data);
-            }
-        }
+        public virtual Task ShowAsync(Func<TViewModel, bool> func)
+            => this.Dispatcher.TrueThreadInvokeAsync(() => Show(func));
 
         /// <summary>
         /// 
@@ -106,7 +123,172 @@ namespace TqkLibrary.WpfUi.ObservableCollections
         /// </summary>
         public override void Save()
         {
-            TriggerEventSave(_datas.Select(x => x.Data));
+            TriggerEventSave(_dict_datas.SelectMany(x => x.Value).Select(x => x.Data));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        protected override void InsertItem(int index, TViewModel item)
+        {
+            this.Dispatcher.VerifyAccess();
+            if (item is null) throw new ArgumentNullException(nameof(item));
+
+            if (this.IsLoaded)
+            {
+                var datas = _EnsureCreateDictKey(item.Data.GroupId);
+
+                if (this.GroupBy(x => x.Data.GroupId).Count() == 1 &&
+                    this.First().Data.GroupId?.Equals(item.Data.GroupId) == true)
+                {
+                    //all items same group
+                    base.InsertItem(index, item);
+
+                    datas.Clear();
+                    datas.AddRange(this);
+                }
+                else
+                {
+                    //search mode, just add to last
+                    datas.Add(item);
+                }
+            }
+            else
+            {
+                //loading
+                base.InsertItem(index, item);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oldIndex"></param>
+        /// <param name="newIndex"></param>
+        protected override void MoveItem(int oldIndex, int newIndex)
+        {
+            if (this.IsLoaded)
+            {
+                if (this.GroupBy(x => x.Data.GroupId).Count() == 1)
+                {
+                    //all same group
+                    base.MoveItem(oldIndex, newIndex);
+
+                    var datas = _EnsureCreateDictKey(this.First().Data.GroupId);
+                    datas.Clear();
+                    datas.AddRange(this);
+                }
+                else
+                {
+                    //search mode => ignore
+                }
+            }
+            else
+            {
+                //loading
+                base.MoveItem(oldIndex, newIndex);
+            }
+            _CleanEmptyDict();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        protected override void RemoveItem(int index)
+        {
+            if (this.IsLoaded)
+            {
+                if (this.GroupBy(x => x.Data.GroupId).Count() == 1)
+                {
+                    var datas = _EnsureCreateDictKey(this.First().Data.GroupId);
+                    //all same group
+                    base.RemoveItem(index);//maybe last items
+
+                    datas.Clear();
+                    datas.AddRange(this);
+                }
+                else
+                {
+                    //search mode
+                    var datas = _EnsureCreateDictKey(this[index].Data.GroupId);
+                    datas.Remove(this[index]);
+                    base.RemoveItem(index);
+                }
+            }
+            else
+            {
+                //loading
+                base.RemoveItem(index);
+            }
+            _CleanEmptyDict();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        protected override void SetItem(int index, TViewModel item)
+        {
+            if (this.IsLoaded)
+            {
+                var datas = _EnsureCreateDictKey(item.Data.GroupId);
+
+                if (this.GroupBy(x => x.Data.GroupId).Count() == 1 &&
+                    this.First().Data.GroupId?.Equals(item.Data.GroupId) == true)
+                {
+                    //all same group
+                    base.SetItem(index, item);
+
+                    datas.Clear();
+                    datas.AddRange(this);
+                }
+                else
+                {
+                    //search mode, ignore
+                }
+            }
+            else
+            {
+                //loading
+                base.SetItem(index, item);
+            }
+            _CleanEmptyDict();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void ClearItems()
+        {
+            if (this.IsLoaded)
+            {
+                if (this.GroupBy(x => x.Data.GroupId).Count() == 1)
+                {
+                    //all same group
+                    var datas = _EnsureCreateDictKey(this.First().Data.GroupId);
+                    datas.Clear();
+                    base.ClearItems();
+                }
+                else
+                {
+                    //search mode
+                    foreach (var item in this)
+                    {
+                        var datas = _EnsureCreateDictKey(item.Data.GroupId);
+                        datas.Remove(item);
+                    }
+                    base.ClearItems();
+                }
+            }
+            else
+            {
+                base.ClearItems();
+            }
+            _CleanEmptyDict();
         }
     }
 }
