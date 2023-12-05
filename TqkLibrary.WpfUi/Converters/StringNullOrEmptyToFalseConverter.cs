b5ac6b0d710a96d626b9609c01cf88e08a0a052d
tqk2811 <tqk2811@gmail.com>
2023-12-05 10:50:17 +0700

    add StringNullOrEmptyToFalseConverter

---
+++ TqkLibrary.WpfUi/Converters/StringNullOrEmptyToFalseConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TqkLibrary.WpfUi.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringNullOrEmptyToFalseConverter : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Result { get; set; } = false;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null || (value is string str && string.IsNullOrWhiteSpace(str))) 
+                return Result;
+            return !Result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
