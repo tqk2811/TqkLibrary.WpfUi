fa8715603acb5661b4879dbab04a022417cbd686
tqk2811 <tqk2811@gmail.com>
2024-07-02 20:08:39 +0700

    add IMainThread

---
+++ TqkLibrary.WpfUi/Interfaces/IMainThread.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Windows.Threading;
+
+namespace TqkLibrary.WpfUi.Interfaces
+{
+    public interface IMainThread
+    {
+        SynchronizationContext SynchronizationContext { get; }
+        Dispatcher Dispatcher { get; }
+    }
+}
