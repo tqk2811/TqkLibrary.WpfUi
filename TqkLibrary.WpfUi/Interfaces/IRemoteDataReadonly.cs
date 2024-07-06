3b852700b097854d9f664f874020227a4e6d8595
tqk2811 <tqk2811@gmail.com>
2024-07-06 17:01:36 +0700

    add IRemoteDataReadonly

---
+++ TqkLibrary.WpfUi/Interfaces/IRemoteDataReadonly.cs
@@ -0,0 +1,16 @@
+﻿#if NET5_0_OR_GREATER
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.WpfUi.Interfaces
+{
+    public interface IRemoteDataReadonly<T>
+    {
+        Task<int> CountAsync(CancellationToken cancellationToken = default);
+        Task<T> GetAsync(T item, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetsAsync(int page, int pageSize, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
+#endif
