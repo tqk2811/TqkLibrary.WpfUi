565503d267e3e48480a97e77bd8c7158ebaf3c99
tqk2811 <tqk2811@gmail.com>
2024-07-06 17:02:41 +0700

    update test

--- TestWpfApp/TestUC/TestAsyncCollection/AsyncData.cs
+++ TestWpfApp/TestUC/TestAsyncCollection/AsyncData.cs
@@ -1,9 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
+using TqkLibrary.WpfUi.Interfaces;
 using TqkLibrary.WpfUi.ObservableCollections.AsyncCollections;
 
 namespace TestWpfApp.TestUC.TestAsyncCollection
 {
-    class AsyncData : IAsyncData<ItemData>
+    class AsyncData : IRemoteDataReadonly<ItemData>
     {
         readonly HashSet<ItemData> _items = new HashSet<ItemData>();
         public AsyncData()
@@ -13,53 +14,28 @@ namespace TestWpfApp.TestUC.TestAsyncCollection
                 _items.Add(new ItemData() { Id = i });
             }
         }
-        public async Task<bool> AddAsync(ItemData item, CancellationToken cancellationToken = default)
-        {
-            await Task.Delay(1000, cancellationToken);
-            return _items.Add(item);
-        }
-        public async Task AddsAsync(IEnumerable<ItemData> items, CancellationToken cancellationToken = default)
-        {
-            await Task.Delay(1000, cancellationToken);
-            foreach (ItemData item in items)
-                _items.Add(item);
-        }
-        public async Task<bool> ClearAsync(CancellationToken cancellationToken = default)
-        {
-            await Task.Delay(1000, cancellationToken);
-            _items.Clear();
-            return true;
-        }
         public async Task<int> CountAsync(CancellationToken cancellationToken = default)
         {
             await Task.Delay(1000, cancellationToken);
             return _items.Count;
         }
-        public async Task<bool> DeleteAsync(ItemData item, CancellationToken cancellationToken = default)
-        {
-            await Task.Delay(1000, cancellationToken);
-            return _items.Remove(item);
-        }
-        public async Task DeletesAsync(IEnumerable<ItemData> items, CancellationToken cancellationToken = default)
-        {
-            await Task.Delay(1000, cancellationToken);
-            foreach (ItemData item in items)
-                _items.Remove(item);
-        }
-        public async Task<IEnumerable<ItemData>> GetsAsync(CancellationToken cancellationToken = default)
+        public async Task<IEnumerable<ItemData>> GetAllAsync(CancellationToken cancellationToken = default)
         {
             await Task.Delay(1000, cancellationToken);
             return _items.ToList();
+        }
+        public async Task<ItemData> GetAsync(ItemData item, CancellationToken cancellationToken = default)
+        {
+            if(_items.Contains(item))
+            {
+                return item;
+            }
+            throw new Exception();
         }
         public async Task<IEnumerable<ItemData>> GetsAsync(int page, int pageSize, CancellationToken cancellationToken = default)
         {
             await Task.Delay(1000, cancellationToken);
             return _items.Skip(page * pageSize).Take(pageSize).ToList();
-        }
-        public async Task<bool> UpdateAsync(ItemData item, CancellationToken cancellationToken = default)
-        {
-            await Task.Delay(1000, cancellationToken);
-            return _items.Contains(item);
         }
     }
 }
--- TestWpfApp/TestUC/TestAsyncCollection/ItemVM.cs
+++ TestWpfApp/TestUC/TestAsyncCollection/ItemVM.cs
@@ -1,17 +1,22 @@
-﻿using TqkLibrary.WpfUi.ObservableCollections;
+﻿using TqkLibrary.WpfUi.Interfaces;
 
 namespace TestWpfApp.TestUC.TestAsyncCollection
 {
-    class ItemVM : IViewModel<ItemData>
+    class ItemVM : IViewModelUpdate<ItemData>
     {
         public ItemVM(ItemData itemData)
         {
             this.Data = itemData;
         }
-        public ItemData Data { get; }
+        public ItemData Data { get; private set; }
 
         public event ChangeCallBack<ItemData>? Change;
 
         public int Id { get { return Data.Id; } }
+
+        public void Update(ItemData data)
+        {
+            this.Data = data ?? throw new ArgumentNullException(nameof(data));
+        }
     }
 }
