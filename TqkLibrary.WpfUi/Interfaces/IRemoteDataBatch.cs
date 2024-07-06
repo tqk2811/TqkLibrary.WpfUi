78c48cdc81ddb44d37ac23ba6b24f9404dbfc1f7
tqk2811 <tqk2811@gmail.com>
2024-07-06 17:02:09 +0700

    add IRemoteDataBatch

---
+++ TqkLibrary.WpfUi/Interfaces/IRemoteDataBatch.cs
@@ -0,0 +1,14 @@
+﻿#if NET5_0_OR_GREATER
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.WpfUi.Interfaces
+{
+    public interface IRemoteDataBatch<T> : IRemoteData<T>
+    {
+        Task<IEnumerable<T>> AddsAsync(IEnumerable<T> items, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> DeletesAsync(IEnumerable<T> items, CancellationToken cancellationToken = default);
+    }
+}
+#endif
